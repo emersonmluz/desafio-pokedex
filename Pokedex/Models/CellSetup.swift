1ea70dd49912656d3a186b1eaff24d14d33632c8
Emerson M Luz <emerson.maciel@vortigo.com.br>
2022-11-18 17:05:25 -0300

    Configurado cores dos tipos dos pokémon's

--- Pokedex/Models/CellSetup.swift
+++ Pokedex/Models/CellSetup.swift
@@ -18,12 +18,23 @@ class CellSetup: UITableViewCell {
     
     @IBOutlet weak var pokemonTypeSecondaryLabel: UILabel!
     
-    func importPokemonBase (_ base: Pokemon, _ index: IndexPath) {
+    func importPokemonBase (_ pokemon: Pokemon, _ index: IndexPath) {
+        
+        pokemonTypePrimaryLabel.layer.cornerRadius = 8
+        pokemonTypeSecondaryLabel.layer.cornerRadius = 8
+        pokemonTypePrimaryLabel.layer.masksToBounds = true
+        pokemonTypeSecondaryLabel.layer.masksToBounds = true
+        
         pokemonImageView.image = UIImage(named: String(format: "%03d", index.row + 1))
-        pokemonNameLabel.text = base.name.english
-        pokemonTypePrimaryLabel.text = base.type[0]
-        if base.type.count == 2 {
-            pokemonTypeSecondaryLabel.text = base.type[1]
+        pokemonNameLabel.text = pokemon.name.english
+        
+        pokemonTypePrimaryLabel.text = pokemon.type[0]
+        pokemonTypePrimaryLabel.backgroundColor = UIColor(named: pokemon.type[0].lowercased())
+        
+        if pokemon.type.count == 2 {
+            pokemonTypeSecondaryLabel.text = pokemon.type[1]
+            pokemonTypeSecondaryLabel.backgroundColor = UIColor(named: pokemon.type[1].lowercased())
+            pokemonTypeSecondaryLabel.isHidden = false
         } else {
             pokemonTypeSecondaryLabel.isHidden = true
         }
