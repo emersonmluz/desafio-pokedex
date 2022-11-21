2af9f2274605bd84b4262c19f575271db85743f2
Emerson M Luz <emerson.maciel@vortigo.com.br>
2022-11-21 14:20:55 -0300

    Nome e tipo na tela de detalhes implementados

--- Pokedex/Controllers/DetailsViewController.swift
+++ Pokedex/Controllers/DetailsViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class DetailsViewController: UIViewController {
     
-    @IBOutlet weak var TopView: UIView!
-    @IBOutlet weak var BottonView: UIView!
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var bottonView: UIView!
     @IBOutlet weak var pokemonImageView: UIImageView!
+    @IBOutlet weak var pokemonNameLabel: UILabel!
+    @IBOutlet weak var pokemonTypePrimaryLabel: UILabel!
+    @IBOutlet weak var pokemonTypeSecondaryLabel: UILabel!
     
     var pokemon: Pokemon?
     var pokemonImageName: String?
@@ -19,9 +22,26 @@ class DetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        TopView.backgroundColor = UIColor(named: pokemon?.type[0].lowercased() ?? "white")
+        topView.backgroundColor = UIColor(named: pokemon?.type[0].lowercased() ?? "white")
         
+        pokemonTypePrimaryLabel.layer.cornerRadius = 5
+        pokemonTypeSecondaryLabel.layer.cornerRadius = 5
+        pokemonTypePrimaryLabel.layer.masksToBounds = true
+        pokemonTypeSecondaryLabel.layer.masksToBounds = true
+        
+        pokemonNameLabel.text = pokemon?.name.english
         pokemonImageView.image = UIImage(named: pokemonImageName ?? "")
+        
+        pokemonTypePrimaryLabel.text = pokemon?.type[0]
+        pokemonTypePrimaryLabel.backgroundColor = UIColor(named: pokemon?.type[0].lowercased() ?? "")
+        
+        if pokemon?.type.count == 2 {
+            pokemonTypeSecondaryLabel.text = pokemon?.type[1]
+            pokemonTypeSecondaryLabel.backgroundColor = UIColor(named: pokemon?.type[1].lowercased() ?? "")
+            pokemonTypeSecondaryLabel.isHidden = false
+        } else {
+            pokemonTypeSecondaryLabel.isHidden = true
+        }
     }
 
 }
